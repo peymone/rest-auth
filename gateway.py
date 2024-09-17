3d544913bd2757719f0a864000f0ef4d14d9596f
Vladimir Reymer <vladimireymer@gmail.com>
2024-09-17 16:43:52 +0700

    Authentification path function and pydantic models for user Reg and Auth

--- gateway.py
+++ gateway.py
@@ -1,34 +1,43 @@
 from fastapi import FastAPI, HTTPException, status
+from fastapi.responses import RedirectResponse
 import requests
 import uvicorn
 
-from models import User
+from models import *
 
-
+# Application setting - remove from here
 app = FastAPI()
+REGISTRATION_SERVICE_URL = 'http://localhost:8002/reg'
+AUTHENTIFICATION_SERVICE_URL = 'http://localhost:8002/auth'
 
 
 @app.post('/reg', status_code=status.HTTP_201_CREATED)
-def register(user: User):
-    """Register user by invoking registrations service via HTTP (RestAPI)
+def register(user: UserReg):
+    """Register user by invoking specific service via HTTP (RestAPI)"""
 
-    Args:
-        user (User): user data
-
-    Returns:
-        json (User) |  json (dict[str]): return user data or error message
-    """
-
-    # user: dict = user.model_dump()
-    response = requests.post('http://localhost:8002/reg', json=user.model_dump(), timeout=10)
+    response = requests.post(REGISTRATION_SERVICE_URL, json=user.model_dump(), timeout=10)
     response = response.json()
 
     if response['result'] == 1:
-        # Make redirect to authentification page. Update Docks
-        return user
+        return RedirectResponse('/auth')
     else:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                             detail="transaction failed when create the User")
+
+
+@app.post('/auth')
+def authentification(user: UserAuth):
+    """Authentificate user by invoking specific service via HTTP(RestAPI)"""
+
+    service_response = requests.post(AUTHENTIFICATION_SERVICE_URL, json=user.model_dump(), timeout=10)
+    service_response = service_response.json()
+
+    if service_response['result'] == 1:
+        return {"operation result": "authentificated"}
+    elif service_response['result'] == 0:
+        return {"operation result": "incorrect password"}
+    else:
+        return {"operation result": "user email is invalid or not exist"}
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,9 +1,16 @@
 from pydantic import BaseModel, Field
 
 
-class User(BaseModel):
-    """User data model"""
+class UserReg(BaseModel):
+    """User data model for registration"""
 
     name: str = Field(min_lenght=2, max_length=20)
     email: str
     password: str = Field(min_length=5)
+
+
+class UserAuth(BaseModel):
+    """User data model for authentification"""
+
+    email: str
+    password: str
