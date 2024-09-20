1c06f10ffa2f23c742861ca54e25229bca3811bf
Vladimir Reymer <vladimireymer@gmail.com>
2024-09-20 15:00:31 +0700

    Updated path functions for reg and auth
    also, added functions for JWT tokens and getting user
    
        removed pydantic models
    
        register - verify password and return user_id
        authentificate - authentificate user and return JWT token
        token_check - verify JWT token
        get_current_user - get current logged in user

--- registration/api.py
+++ registration/api.py
@@ -1,32 +1,24 @@
-from pydantic import BaseModel
-from fastapi import FastAPI, status
+from fastapi import FastAPI
 import uvicorn
 
+from models import *
 from db import add_user as add_user_to_db
-from db import get_password as get_password_from_db
-from security import hash_password, verify_password
+from db import get_user_by_name, get_user_by_id
+from security import hash_password, verify_password, generate_token, verify_token
 
 
 app = FastAPI()
 
 
-class UserReg(BaseModel):
-    """User data model for registration"""
+@app.post('/verify_token')
+def token_check(token: dict) -> dict:
 
-    name: str
-    email: str
-    password: str
-
-
-class UserAuth(BaseModel):
-    """User data model for authentification"""
-
-    email: str
-    password: str
+    user_id = verify_token(token['access_token'])
+    return {'user_id': user_id}
 
 
 @app.post('/reg')
-def register(user: UserReg):
+def register(user: UserReg) -> dict:
     """Register new User by adding to Data Base
 
     Args:
@@ -35,32 +27,52 @@ def register(user: UserReg):
         password (str): user password
 
     Returns:
-        json (int): return 1 if User added successfully or 0 otherwise
+        dict (int | None): return user_id if User added successfully or None otherwise
     """
 
     # Hash password and add User data to DB
     hashed_password = hash_password(user.password)
-    result: bool = add_user_to_db(user.name, user.email, hashed_password)
+    user_id: int | None = add_user_to_db(user.name, user.email, hashed_password)
 
-    if result is True:
-        return {"result": 1}
+    # Return user id
+    if user_id is None:
+        return {'user_id': None}
     else:
-        return {"result": 0}
+        return {'user_id': user_id}
 
 
 @app.post('/auth')
-def authentificate(user: UserAuth):
-    user_data = get_password_from_db(user.email)
+def authentificate(user: UserAuth) -> Token:
+    """Authentificate user and return JWT token
+
+    Args:
+        user (UserAuth): user.name (str), user.password (str)
+
+    Returns:
+        Token: Token.access_token (str), Token.token_type (str)
+    """
+
+    user_data = get_user_by_name(user.name)
 
     if user_data is None:
-        return {'result': "user email is invalid or not exist"}
+        return {'access_token': None}
     else:
-        if verify_password(user.password, user_data[0]):
-            return {'result': 1}
+        user_id, user_email, user_password = user_data
+
+        if verify_password(user.password, user_password) is True:
+            jwt_token = generate_token({'sub': user_id})
+            return Token(access_token=jwt_token)
         else:
-            return {'result': 0}
+            return NoToken
+
+
+@app.get('/user')
+def get_current_user(user_id: int) -> UserGet:
+    user = get_user_by_id(user_id)
+    user_name, user_email, user_password = user
+
+    return UserGet(id=user_id, name=user_name, email=user_email)
 
 
 if __name__ == '__main__':
-    # For testing only
     uvicorn.run(app, host='localhost', port=8002)
