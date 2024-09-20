cac970e4dd2bbc8e53b00fa552b1ae8e88466a1e
Vladimir Reymer <vladimireymer@gmail.com>
2024-09-20 14:53:10 +0700

    Add JWT Token generating and verifying
        generate_token - generate JWT Token. PyJWT module
        verify_token - verify token signature

--- registration/security.py
+++ registration/security.py
@@ -1,5 +1,14 @@
 from passlib.context import CryptContext
+import jwt
+from jwt.exceptions import InvalidTokenError, ExpiredSignatureError
 
+from datetime import datetime, timedelta, timezone
+
+
+# Remove it from here
+SECRET_KEY = "some_really_secret_key"
+ALGHORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 crypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
@@ -31,3 +40,33 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
     result = crypt_context.verify(plain_password, hashed_password)
     return result
+
+
+def generate_token(user_data: dict) -> str:
+    payload = user_data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload.update({'exp': expire})
+    encoded_jwt = jwt.encode(payload, SECRET_KEY, ALGHORITHM)
+
+    return encoded_jwt
+
+
+def verify_token(encoded_jwt: str) -> int:
+
+    try:
+
+        payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=[ALGHORITHM])
+        user_id: int = payload.get('sub')
+
+        if user_id is None:
+            return 0
+
+        return user_id
+
+    except InvalidTokenError:
+        print("Token data is carrupted")
+        return 0
+
+    except ExpiredSignatureError:
+        print("Token is expired")
+        return 0
